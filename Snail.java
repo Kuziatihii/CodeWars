42088a486aa8aa5c0fcabca1ce30a3b88bda3a75
Kuziatihii <61865176+Kuziatihii@users.noreply.github.com>
2021-04-28 14:36:52 +0300

    Comments

--- Snail.java
+++ Snail.java
@@ -1,64 +1,73 @@
+/*
+Snail Sort
+Given an n x n array, return the array elements arranged from outermost elements 
+to the middle element, traveling clockwise.
+array = [[1,2,3],
+         [4,5,6],
+         [7,8,9]]
+snail(array) #=> [1,2,3,6,9,8,7,4,5]
+*/
 public class Snail {
-  public static int[] snail(int[][] array) {
-    if(array[0].length == 0){
-      return new int[0];
-    }
-    int[] result =new int [array.length * array.length];
-    String dir = "right";
-    int imin = 0;
-    int imax = array.length - 1;
-    int jmin = 0;
-    int jmax = array.length - 1;
-    int n = 0;
-    int i = imin;
-    int j = jmin;
-    boolean done = false;
-    while (!done) {
-      switch (dir) {
-        case "right":                
-          i = imin;
-          j = jmin;
-          for (; j <= jmax; j++){
-            result[n] = array[i][j];
-            n++;
-          }
-          dir = "down";
-          imin++;
-          break;
-        case "left":
-          i = imax;
-          j = jmax;                
-          for (; j >= jmin; j--){
-            result[n] = array[i][j];
-            n++;
-          }
-          dir = "up";
-          imax--;
-          break;
-        case "down":
-          i = imin;
-          j = jmax;
-          for (; i <= imax; i++){
-            result[n] = array[i][j];
-            n++;
-          }
-          dir = "left";
-          jmax--;
-          break;
-        case "up":
-          i = imax;
-          j = jmin;
-          for (; i >= imin; i--){
-            result[n] = array[i][j];
-            n++;
-          }
-          dir = "right";
-          jmin++;
-          break;
-      }
-      if (imin > imax || jmin > jmax)
-        done = true;
-    }
-    return result;
-  } 
+	public static int[] snail(int[][] array) {
+		if(array[0].length == 0){
+			return new int[0];
+		}
+		int[] result =new int [array.length * array.length];
+		String dir = "right";
+		int imin = 0;
+		int imax = array.length - 1;
+		int jmin = 0;
+		int jmax = array.length - 1;
+		int n = 0;
+		int i = imin;
+		int j = jmin;
+		boolean done = false;
+		while (!done) {
+			switch (dir) {
+				case "right":                
+				i = imin;
+				j = jmin;
+				for (; j <= jmax; j++){
+					result[n] = array[i][j];
+					n++;
+				}
+				dir = "down";
+				imin++;
+				break;
+				case "left":
+				i = imax;
+				j = jmax;
+				for (; j >= jmin; j--){
+					result[n] = array[i][j];
+					n++;
+				}
+				dir = "up";
+				imax--;
+				break;
+				case "down":
+				i = imin;
+				j = jmax;
+				for (; i <= imax; i++){
+					result[n] = array[i][j];
+					n++;
+				}
+				dir = "left";
+				jmax--;
+				break;
+				case "up":
+				i = imax;
+				j = jmin;
+				for (; i >= imin; i--){
+					result[n] = array[i][j];
+					n++;
+				}
+				dir = "right";
+				jmin++;
+				break;
+			}
+			if (imin > imax || jmin > jmax)
+				done = true;
+		}
+		return result;
+	} 
 }
